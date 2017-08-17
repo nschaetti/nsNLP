aaa376b6dee64e9bd24a904ba78241156ac645ff
nschaetti <n.schaetti@gmail.com>
2017-08-17 21:21:57 +0200

    Cross validation

---
+++ __init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# File : __init__.py
+# Description : .
+# Date : 16/08/2017
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+
+# Imports
+import classifiers
+import deep_models
+import esn_models
+import lsa_models
+import lstm_models
+import rnn_models
+import statistical_models
+import tfidf
+import validation
+
+__all__ = ['classifiers', 'deep_models', 'esn_models', 'lsa_models', 'lstm_models', 'rnn_models', 'statistical_models', 'tfidf', 'validation']
