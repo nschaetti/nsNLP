1e71c7c2ff64346b63d922a03ad5792784f6095f
nschaetti <n.schaetti@gmail.com>
2017-09-16 20:14:51 +0200

    TFIDF initial model

---
+++ statistical_models/TFIDFTextClassifier.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : statistical_models.SLTextClassifier.py
+# Description : Statistical language text classifier.
+# Auteur : Nils Schaetti <nils.schaetti@unine.ch>
+# Date : 01.02.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the nsNLP toolbox.
+# The RnsNLP toolbox is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Imports
+import spacy
+from sys import getsizeof
+import decimal
+from nsNLP.classifiers.TextClassifier import TextClassifier
+
+
+# TF-IDF text classifier
+class TFIDFTextClassifier(TextClassifier):
+    pass
+# end TDIDFTextClassifier
