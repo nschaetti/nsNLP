597d9c8eb232ed0b348fefacd32133cb180123b1
nschaetti <n.schaetti@gmail.com>
2017-09-16 20:04:41 +0200

    Bag of trigrams

---
+++ features/BagOf3Grams.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#
+# File : core/downloader/PySpeechesConfig.py
+# Description : .
+# Date : 20th of February 2017
+#
+# This file is part of pySpeeches.  pySpeeches is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+
+# Import packages
+
+
+# Bag of 3 grams
+class BagOf3Grams(object):
+    """
+    Bag of 3-grams
+    """
+
+    # Constructor
+    def __init__(self, uppercase=False):
+        """
+        Constructor
+        :param text:
+        """
+        self._uppercase = uppercase
+        self._voc_count = dict()
+    # end __init__
+
+    #########################################
+    # Public
+    #########################################
+
+    #########################################
+    # Override
+    #########################################
+
+    # Call
+    def __call__(self, tokens):
+        """
+        Call
+        :return:
+        """
+        # Preceding tokens
+        last_token1 = None
+        last_token2 = None
+
+        # For each tokens
+        for token in tokens:
+            # Uppercase?
+            if not self._uppercase:
+                token = token.lower()
+            # end if
+
+            if last_token2 is None:
+                last_token2 = token
+            if last_token1 is None:
+                last_token1 = token
+            else:
+                # Bigram
+                bigram = last_token2 + u" " + last_token1 + u" " + token
+
+                # Add
+                try:
+                    self._voc_count[bigram] += 1.0
+                except KeyError:
+                    self._voc_count[bigram] = 1.0
+                # end try
+
+                # Preceding tokens
+                last_token2 = last_token1
+                last_token1 = token
+            # end if
+        # end for
+
+        return self._voc_count
+    # end __call__
+
+    #########################################
+    # Private
+    #########################################
+
+# end BagOfWords
