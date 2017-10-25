562238a8680ee10b607eb89ed5b6aca9a2020461
Nils Schaetti <nils.schaetti@unine.ch>
2017-10-25 14:29:38 +0200

    A converter to join to converters

---
+++ esn_models/converters/JoinConverter.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : core.classifiers.RCNLPContainerConverter.py
+# Description : Echo State Network for text classification.
+# Auteur : Nils Schaetti <nils.schaetti@unine.ch>
+# Date : 01.02.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the Reservoir Computing NLP Project.
+# The Reservoir Computing Memory Project is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import numpy as np
+from Converter import Converter
+import OneHotConverter
+import LetterConverter
+
+
+# Join two converters
+class JoinConverter(Converter):
+    """
+    Join two converters
+    """
+
+    # Constructor
+    def __init__(self, conv1, conv2):
+        """
+        Constructor
+        :param conv1: First converter
+        :param conv2: Second converter
+        :param lang: Language model
+        :param tag_to_symbol: Tag to symbol conversion array.
+        :param resize: Reduce dimensionality.
+        """
+        super(JoinConverter, self).__init__()
+
+        # Check that there is not One-Hot or Letter converter
+        if type(conv1) is OneHotConverter.OneHotConverter or type(conv1) is LetterConverter.LetterConverter or \
+            type(conv2) is OneHotConverter.OneHotConverter or type(conv2) is LetterConverter.LetterConverter:
+            raise Exception(u"Cannot join with a converter of type OneHot or Letter")
+        # end if
+
+        # Converters
+        self._conv1 = conv1
+        self._conv2 = conv2
+    # end __init__
+
+    ##############################################
+    # Public
+    ##############################################
+
+    ##############################################
+    # Override
+    ##############################################
+
+    # Convert a string to a ESN input
+    def __call__(self, text, exclude=list(), word_exclude=list()):
+        """
+        Convert a string to a ESN input
+        :param text: The text to convert.
+        :return: An numpy array of inputs.
+        """
+
+        # Converted text 1
+        conv1_text = self._conv1(text, exclude, word_exclude)
+
+        # Converted text 2
+        conv2_text = self._conv2(text, exclude, word_exclude)
+
+        return np.hstack((conv1_text, conv2_text))
+    # end convert
+
+    ##############################################
+    # Private
+    ##############################################
+
+    # Get the number of inputs
+    def _get_inputs_size(self):
+        """
+        Get the input size.
+        :return: The input size.
+        """
+        return self._conv1.get_n_inputs() + self._conv2.get_n_inputs()
+    # end get_n_inputs
+
+# end RCNLPConverter
