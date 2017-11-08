03dd41f98a307a0070a5dc0c6c86d67a012757e1
Nils Schaetti <nils.schaetti@unine.ch>
2017-11-08 16:57:33 +0100

    Clustering evaluation

---
+++ measures/Clustering.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+
+# Clustering evaluation
+class Clustering(object):
+    """
+    Clustering measure
+    """
+
+    #################################
+    # Static
+    #################################
+
+    # B-cubed F-1
+
+# end Clustering
