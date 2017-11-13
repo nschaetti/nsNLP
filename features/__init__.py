a7a73076292264e6fd5546a72d509fa237868482
nschaetti <n.schaetti@gmail.com>
2017-11-13 23:20:53 +0100

    init

--- features/__init__.py
+++ features/__init__.py
@@ -1,7 +1,7 @@
 
 # Imports
-#from .LettersMatrix import LettersMatrix
-#from .LetterStatistics import LetterStatistics
+from .LettersMatrix import LettersMatrix
+from .LetterStatistics import LetterStatistics
 from .BagOfWords import BagOfWords
 from .BagOf2Grams import BagOf2Grams
 from .BagOf3Grams import BagOf3Grams
