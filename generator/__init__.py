a1865a4369feaf3c3a452ce14bace7402bfc82f1
nschaetti <n.schaetti@gmail.com>
2017-12-05 15:09:55 +0100

    Text generator abstract class

---
+++ generator/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# File : __init__.py
+# Description : .
+# Date : 05/08/2017
+#
+# Copyright Nils Schaetti, University of Neuchâtel <nils.schaetti@unine.ch>
+
+from .TextGenerator import TextGenerator
