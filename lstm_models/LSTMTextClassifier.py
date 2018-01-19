5b8e9afbe368d31fd33b76d071304a54f5641e38
nschaetti <n.schaetti@gmail.com>
2018-01-19 12:09:27 +0100

    LSTM classifier

--- lstm_models/LSTMTextClassifier.py
+++ lstm_models/LSTMTextClassifier.py
@@ -25,9 +25,12 @@
 # Import packages
 from sys import getsizeof
 from nsNLP.classifiers.TextClassifier import TextClassifier
+import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.autograd as autograd
 from .modules.LSTMModule import LSTMModule
+from decimal import Decimal
 
 
 # LSTM classifier model
@@ -36,8 +39,14 @@ class LSTMTextClassifier(TextClassifier):
     LSTM classifier model
     """
 
+    # Properties
+    _model = None
+    _loss_function = None
+    _optimizer = None
+    _last_y = None
+
     # Constructor
-    def __init__(self, classes, hidden_size, converter, embedding_dim=300, learning_rate=0.1):
+    def __init__(self, classes, hidden_size, converter, embedding_dim=300, learning_rate=0.1, voc_size=0, n_epoch=300, aggregation='average', smoothing=0.001):
         """
         Constructor
         :param classes:
@@ -52,15 +61,15 @@ class LSTMTextClassifier(TextClassifier):
         # Properties
         self._embedding_dim = embedding_dim
         self._hidden_size = hidden_size
+        self._n_epoch = n_epoch
+        self._converter = converter
+        self._voc_size = voc_size
+        self._learning_rate = learning_rate
+        self._aggregation = aggregation
+        self._smoothing = smoothing
 
         # Create model
-        if converter is not None:
-            self._model = LSTMModule(embedding_dim, hidden_size, self._n_classes)
-        else:
-            self._model = LSTMModuleEmbeddings()
-        # end if
-        self._loss_function = nn.NLLLoss()
-        self._optimizer = optim.SGD(self._model.parameters(), lr=learning_rate)
+        self._reset_model()
     # end __init__
 
     ##############################################
@@ -107,7 +116,31 @@ class LSTMTextClassifier(TextClassifier):
         Finalize the training phase
         :param verbose: Verbosity
         """
-        pass
+        # For each epoch
+        for epoch in range(self._n_epoch):
+            # For each training text file
+            for index, (x, y) in enumerate(self._examples):
+                # As pyTorch accumulates gradients we clear them
+                # out before each instance.
+                self._model.zero_grad()
+
+                # Re-init the hidden state
+                self._model.hidden = self._model.init_hidden()
+
+                # Now we prepare the inputs and turn them into
+                # variables.
+                class_index = self._class_to_int(y)
+                train_x, train_y = self._generate_training_data(x, class_index)
+
+                # Run the forward pass
+                tag_scores = self._model(x)
+
+                # Compute loss, gradients and update parameters
+                loss = self._loss_function(tag_scores, train_y)
+                loss.backward()
+                self._optimizer.step()
+            # end for
+        # end for
     # end _finalize_training
 
     # Classify a text file
@@ -117,7 +150,57 @@ class LSTMTextClassifier(TextClassifier):
         :param text: Text to classify
         :return: Predicted class and class probabilities
         """
-        pass
+        # Get inputs
+        x = self._generate_test_data(text)
+
+        # Get class scores
+        class_scores = self._model(x)
+
+        # Normalized
+        y = class_scores - torch.min(class_scores)
+        y /= torch.max(class_scores)
+
+        # Save last y
+        self._last_y = y
+
+        # Get maximum probability class
+        if self._aggregation == 'average':
+            outputs_average = torch.mean(y, 0)
+            _, best_class = torch.max(outputs_average, 0)
+            return self._int_to_class(best_class), outputs_average
+        elif self._aggregation == 'multiply':
+            # Decimal score
+            scores = list()
+            for i in range(self._n_classes):
+                scores.append(Decimal(1.0))
+            # end for
+
+            # For each outputs
+            for pos in range(y.shape[0]):
+                for i in range(self._n_classes):
+                    if y[pos, i] == 0.0:
+                        scores[i] = scores[i] * Decimal(self._smoothing)
+                    else:
+                        scores[i] = scores[i] * Decimal(y[pos, i])
+                    # end if
+                # end for
+            # end for
+
+            # Return the max
+            max = 0.0
+            max_c = None
+            for i in range(self._n_classes):
+                if scores[i] > max:
+                    max_c = self._int_to_class(i)
+                    max = scores[i]
+                # end if
+            # end for
+            return max_c, scores
+        elif self._aggregation == 'last':
+            last_outputs = y[-1]
+            _, best_class = torch.max(last_outputs, 0)
+            return self._int_to_class(best_class), last_outputs
+        # end if
     # end _classify
 
     # Reset learning but keep reservoir
@@ -125,18 +208,46 @@ class LSTMTextClassifier(TextClassifier):
         """
         Reset model learned parameters
         """
-        pass
+        # Create model
+        if self._converter is not None:
+            self._model = LSTMModule(self._converter.get_n_inputs(), self._hidden_size, self._n_classes)
+        else:
+            self._model = LSTMModule(self._embedding_dim, self._hidden_size, self._n_classes, self._voc_size, train_embeddings=True)
+        # end if
+
+        # Loss function and optimizer
+        self._loss_function = nn.NLLLoss()
+        self._optimizer = optim.SGD(self._model.parameters(), lr=self._learning_rate)
     # end _reset_model
 
     # Generate training data from text
-    def _generate_training_data(self, text, author):
+    def _generate_training_data(self, text, c):
         """
         Generate training data from text file.
         :param text: Text
-        :param author: Corresponding author.
+        :param c: Corresponding class.
         :return: Data set inputs
         """
-        pass
+        # Get Temporal Representations
+        reps = self._converter(text)
+
+        # Converter type
+        converter_type = type(self._converter)
+
+        # Generate x and y
+        x, _ = converter_type.generate_data_set_inputs(reps, self._n_classes, c)
+
+        # Training length
+        train_length = x.shape[0]
+
+        # Targets as tensor
+        y = torch.FloatTensor(train_length, self._n_classes).zero_()
+        y[:, c] = 1
+
+        # Inputs as Tensor
+        x = torch.FloatTensor(x)
+
+        return autograd.Variable(x), autograd.Variable(y)
     # end generate_training_data
 
     # Generate text data from text file
@@ -146,7 +257,11 @@ class LSTMTextClassifier(TextClassifier):
         :param text: Text
         :return: Test data set inputs
         """
-        pass
+        # Get inputs
+        x = self._converter(text)
+
+        # Convert to Tensor
+        return autograd.Variable(torch.FloatTensor(x))
     # end generate_text_data
 
 # end LSTMTextClassifier
